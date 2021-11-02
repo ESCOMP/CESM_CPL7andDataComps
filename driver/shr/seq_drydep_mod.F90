1e34c251383bb9280c54076b341fc412faf3ea99
Francis Vitt <fvitt@ucar.edu>
2021-11-02 17:51:43 -0600

    correction to how lnd_drydep in implemented
            modified:   driver/shr/seq_drydep_mod.F90

--- driver/shr/seq_drydep_mod.F90
+++ driver/shr/seq_drydep_mod.F90
@@ -1,6 +1,5 @@
 module seq_drydep_mod
 
-  use shr_drydep_mod, only: seq_drydep_readnl=>shr_drydep_readnl
   use shr_drydep_mod, only: seq_drydep_setHCoeff=>shr_drydep_setHCoeff
   use shr_drydep_mod
 
@@ -9,11 +8,20 @@ module seq_drydep_mod
   ! method specification
   character(len=*), parameter :: DD_XLND = 'xactive_lnd' ! dry-dep land
   character(len=*), parameter :: drydep_method = DD_XLND ! XLND is the only option now
+  logical, protected :: lnd_drydep
 
 contains
 
-  logical function lnd_drydep
+  subroutine seq_drydep_readnl(NLFilename, ID, shr_drydep_fields)
+
+    character(len=*), intent(in)  :: NLFilename ! Namelist filename
+    integer         , intent(in)  :: ID         ! seq_comm ID
+    character(len=*), intent(out) :: shr_drydep_fields
+
+    call shr_drydep_readnl(NLFilename, ID, shr_drydep_fields)
+
     lnd_drydep = n_drydep>0
-  end function lnd_drydep
+
+  end subroutine seq_drydep_readnl
 
 end module seq_drydep_mod
